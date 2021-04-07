841be90b2ed41bfeb05dd982eab131ef2ef75765
wasabi-neko <wasabi.neko.owo@gmail.com>
2021-04-07 22:30:52 +0800

    Delete dummy

--- my-shell.c
+++ my-shell.c
@@ -185,7 +185,6 @@ int prompt()
     if (getcwd(cwd, sizeof(cwd)) != 0) {
         printf("%s", cwd);
     }
-    printf("aaa:");
     printf("\033[0;37m");       /* Back to white */
     printf("$ ");
 
